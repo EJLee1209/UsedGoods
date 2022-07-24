b4a7fea5f04b721e8a12c0d8d6c760ea3b926afb
EJLee1209 <dldmswo1209@gmail.com>
2022-07-24 17:27:30 +0900

    init RecyclerView

---
+++ app/src/main/java/com/dldmswo1209/usedgoods/home/ArticleModel.kt
@@ -0,0 +1,9 @@
+package com.dldmswo1209.usedgoods.home
+
+data class ArticleModel (
+    val sellerId: String,
+    val title: String,
+    val createdAt: Long,
+    val price: String,
+    val imageUrl: String
+)
--- app/src/main/java/com/dldmswo1209/usedgoods/home/HomeFragment.kt
+++ app/src/main/java/com/dldmswo1209/usedgoods/home/HomeFragment.kt
@@ -6,11 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dldmswo1209.usedgoods.MainActivity
 import com.dldmswo1209.usedgoods.R
+import com.dldmswo1209.usedgoods.databinding.ActivityMainBinding
+import com.dldmswo1209.usedgoods.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var mBinding: FragmentHomeBinding? = null
+    private val binding get() = mBinding!!
+    private lateinit var articleAdapter: ArticleAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mBinding = FragmentHomeBinding.bind(view)
+
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply {
+            add(ArticleModel("0","aaaa",1000000, "5000원",""))
+            add(ArticleModel("0","bbbb",2000000, "1000원",""))
+        })
+
+        binding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.articleRecyclerView.adapter = articleAdapter
+
     }
 
 }
